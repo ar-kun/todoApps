05667301b2d71251ee5c9c28fb6751e073409625
Adam Ramdan <arshinks12@gmail.com>
2023-07-22 07:39:12 +0700

    Develop todo toggle

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, toggleCompleted }) => {
+ const getTodoTitleStyle = () => {
+  if (todo.completed === true) {
+   return { textDecoration: 'line-through' };
+  } else {
+   return { textDecoration: 'none' };
+  }
+ };
+
  return (
-  <div style={style}>
-   <p>{todo.title}</p>
+  <div style={styles.todoItem}>
+   {/* Panggil function getTodoTitleStyle */}
+   <input
+    type="checkbox"
+    style={styles.checkbox}
+    // Jalankan function toggleCompleted ketika checkbox di-klik
+    onChange={() => toggleCompleted(todo.id)}
+   />
+   <p style={getTodoTitleStyle()}>{todo.title}</p>
   </div>
  );
 };
 
-const style = {
- border: '2px solid #f4f4f4',
- fontSize: '24px',
+const styles = {
+ todoItem: {
+  border: '2px solid #f4f4f4',
+  fontSize: '24px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+ },
+ checkbox: {
+  marginRight: '10px',
+  height: '18px',
+  width: '18px',
+ },
 };
 
 export default TodoItem;
